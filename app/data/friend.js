d9430fc8715d34c7ca721119a1d28cb49740e0a4
nwdehmlow <nwdehmlow@gmail.com>
2017-11-27 17:27:08 -0600

    final update

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+
+var friendList = [
+  {
+    name: 'Nick Dehmlow',
+    profilePic: 'https://avatars3.githubusercontent.com/u/25062283?s=400&u=8e3d790ab0f1c3e09264487ab99c730c84145301&v=4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,38 @@
+//Load Data
+var friendList = require('../data/friend.js');
+
+module.exports = function(app){
+
+  app.get('/api/friends', function(req,res){
+    res.json(friendList);
+  });
+
+  app.post('/api/friends', function(req,res){
+
+    var newFriendScores = req.body.scores;
+    var scoresArray = [];
+    var friendCount = 0;
+    var bestMatch = 0;
+
+    for(var i=0; i<friendList.length; i++){
+      var totalDifference = 0;
+
+      for(var j=0; j<newFriendScores.length; j++){
+        totalDifference += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
+
+      scoresArray.push(totalDifference);
+    }
+
+    for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
+      }
+    }
+
+    var bff = friendList[bestMatch];
+    res.json(bff);
+
+    friendList.push(req.body);
+  });
+};
